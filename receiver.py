38e5c6e2c8209d2edf07d97063316fca433e6654
Ack008 <67114515+Ack008@users.noreply.github.com>
2022-07-31 17:43:53 +0200

    Update receiver.py

--- receiver.py
+++ receiver.py
@@ -1,3 +1,30 @@
 import socket as sk
 import os
 import pyttsx3
+import sys
+
+ascoltanto = True
+def ricevi(conn : sk.socket):
+    while ascoltanto:
+        recv = conn.recv(4096).decode()
+        if recv == "shut":
+            ascoltanto = False
+    os.system("shutdown /s /t 1")
+    
+
+
+
+id = sk.gethostbyname(sk.gethostname())
+port = 9999
+riuscita = True
+try:
+    socket = sk.socket()
+    socket.bind((id,port))
+    socket.listen(1)
+except sk.error as er:
+    print(f"Qualcosa è andato storto... {er}")
+    riuscita = False
+
+if riuscita:
+    conn, add = socket.accept()
+    
